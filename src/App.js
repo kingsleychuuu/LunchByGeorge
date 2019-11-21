5620fc86f9a3c3086f48426a59a51c23390e6a1e
Amean Asad <ameanasad@Ameans-MacBook-Pro.local>
2019-11-21 15:21:10 -0500

    Terminal Push

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the site for Lunch by George.
+          This is the site for Lunch by George
         </p>
         <a
           className="App-link"
