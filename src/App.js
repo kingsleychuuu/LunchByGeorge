80c6305ebef4fabc070d2e341f7c3c58809848be
Amean Asad <ameanasad@Ameans-MacBook-Pro.local>
2019-11-23 15:10:57 -0500

    Added router and setup page files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Navigation from "./components/navigation";
+
+import Home from "./components/home";
+
+import About from "./components/about";
+import Partners from "./components/partners";
+
+import Volunteer from "./components/volunteer";
+import {
+  //  Replace Hash with BrowserRouter if server fails
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/partners" exact component={Partners} />
+          <Route path="/volunteer" exact component={Volunteer} />
+        </Switch>
+      </Router>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
