fbecadafc92b0e4ce52e67daffcb57058ca6bf61
Amean Asad <ameanasad@Ameans-MacBook-Pro.local>
2019-11-23 14:21:20 -0500

    Added Component Files

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Footer extends Component {
+    state = {}
+    render() {
+        return (<div>
+
+            <h> Test</h>
+        </div>);
+    }
+}
+
+export default Footer;
