89ec7a0ff3cbbcbb21d2487c38da9d178fad7904
Monica Stuart <15mxs1@caslab.queensu.ca>
2020-01-06 23:53:11 -0500

    added logistics and google maps to home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import Carousel from "react-bootstrap/Carousel";
 
 import "../scss/home.scss"
-
+import * as Text from ".././strings.json";
 import CarouselItem from "react-bootstrap/CarouselItem";
 
 import Row from "react-bootstrap/Row";
@@ -13,6 +13,13 @@ import Col from "react-bootstrap/Col";
 
 class Home extends Component {
     render() {
+        const businessHoursHeader=Text.logistics.businessHoursHeader;
+        const businessHours=Text.logistics.businessHours;
+        const mealSchedule=Text.logistics.mealSchedule;
+        const mealOne=Text.logistics.mealOne;
+        const mealTwo=Text.logistics.mealTwo;
+        const mealThree=Text.logistics.mealThree;
+
         return (
             <main>
                 <div class="carousel-wrapper">
@@ -76,10 +83,25 @@ class Home extends Component {
                     </Carousel>
 
                 </div>
+                <div className="container-logistics">
+                    <div className="logistics">
+                        <h2 className="heading-business-hours">{businessHoursHeader.toUpperCase()}</h2>
+                        <p>{businessHours}</p>
+                        <ul>
+                        <h3 className="meal-schedule">{mealSchedule.toUpperCase()}</h3>
 
+                        <li>{mealOne}</li>
+                        <li>{mealTwo}</li>
+                        <li>{mealThree}</li>
+                        </ul>
+                        </div>
+                        <div className="google-maps">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2858.9040726652975!2d-76.48542948420614!3d44.22963332298199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cd2aafeee04ff03%3A0x6a19edc061e8708e!2s129%20Wellington%20St%2C%20Kingston%2C%20ON%20K7L%203C9!5e0!3m2!1sen!2sca!4v1578370682281!5m2!1sen!2sca" width="100%" height="100%" frameborder="0" style={{border:0}} allowfullscreen=""></iframe>
+            </div>
+            </div>
             </main>
         );
-    }
+}
 }
 
 export default Home;
